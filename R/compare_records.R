bac13ee12e258951b4709a3e440a664662ec697d
briankundinger <briankundinger@gmail.com>
2024-03-06 15:23:37 -0500

    compare_records

--- R/compare_records.R
+++ R/compare_records.R
@@ -1,14 +1,42 @@
-compare_records_BK <- function(df_1, df_2, fields,
+compare_records <- function(df_1, df_2, fields,
                                fields_1 = fields, fields_2 = fields,
                                types = rep("bi", length(fields)),
-                               breaks_lv = c(0,.25,.5), breaks_nu = c(0, 1)){
+                               breaks = rep(list(NA), length(types)),
+                               distance_metric = "Levenshtein"){
 
   n1 <- nrow(df_1)
   n2 <- nrow(df_2)
-  F <- length(fields)
+  FF <- length(fields)
 
-  breaks_lv <- unique(c(-Inf, breaks_lv, Inf))
-  breaks_nu <- unique(c(-Inf, breaks_nu, Inf))
+  if(typeof(breaks) == "double"){
+    breaklist <- rep(list(-Inf, breaks, Inf), length(types))
+  }
+
+  if(typeof(breaks) == "list"){
+    breaklist <- rep(list(NA), length(types))
+
+    for(f in 1:F){
+      if(types[f] == "lv" & breaks[[f]] == NA){
+        breaklist[[f]] <- c(0, .25, .5)
+      }
+      if(types[[f]] == "lv"){
+        breaklist[f]] <- c(-Inf, breaks[[f]], Inf)
+      }
+      if(types[f] == "nu" & breaks[[f]] == NA){
+        breaklist[[f]] <- c(0, 1)
+      }
+      if(types[[f]] == "nu"){
+        breaklist[[f]] <- c(-Inf, breaks[[f]], Inf)
+      }
+
+    }
+
+  }
+
+
+
+  # breaks_lv <- unique(c(-Inf, breaks_lv, Inf))
+  # breaks_nu <- unique(c(-Inf, breaks_nu, Inf))
 
   ids <- expand.grid(1:n1, 1:n2)
   ids_1 <- ids[, 1]
@@ -17,23 +45,27 @@ compare_records_BK <- function(df_1, df_2, fields,
   ohe <- vector(mode = "list", length = F)
 
   # FS agreement levels
-  for(f in 1:F){
+  for(f in 1:FF){
     if(types[f] == "bi"){
-      comp <- matrix(1, nrow = dim(ids)[1], ncol = 2)
       comp <- df_1[ids_1, fields_1[f]] == df_2[ids_2, fields_2[f]]
       comp <- (!comp) + 1
       comparisons[[f]] <- factor(comp)
     }
     if(types[f] == "lv"){
-
+      if(distance_metric == "Levenshtein"){
+        distance <- RecordLinkage::levenshteinDist(as.character(df_1[ids_1, fields_1[f]]),
+                                       as.character(df_2[ids_2, fields_2[f]]))
+      }
+      if(distance_metric == "Damerau-Levenshtein"){
       # Damerau-Levenshtein distance, so transpositions count as 1.
       # In contrast, BRL uses standard Levenshtein, so transpositions count as 2
       distance <- 1 - levitate::lev_ratio(as.character(df_1[ids_1, fields_1[f]]),
                                           as.character(df_2[ids_2, fields_2[f]]),
                                           useNames = F)
+      }
 
       comp <- cut(distance,
-                  breaks = breaks_lv) %>%
+                  breaks = breaklist[[f]]) %>%
         as.integer() %>%
         factor()
       comparisons[[f]] <- comp
@@ -43,7 +75,7 @@ compare_records_BK <- function(df_1, df_2, fields,
     if(types[f] == "nu"){
       distance <- abs(df_1[ids_1, fields_1[f]] - df_2[ids_2, fields_2[f]])
       comp <- cut(distance,
-                  breaks = breaks_nu,
+                  breaks = breaklist[[f]],
                   include.lowest = T) %>%
         as.integer()
       comparisons[[f]] <- comp
@@ -55,7 +87,7 @@ compare_records_BK <- function(df_1, df_2, fields,
 
   # Convert to one-hot encoding
   # TODO: Test for speed vs onehot encoding function, or lapply
-  for(f in 1:F){
+  for(f in 1:FF){
     ohe[[f]] <- matrix(0, nrow = n1 * n2, ncol = n_levels[f])
     for(ell in 1:n_levels[f]){
       ohe[[f]][comparisons[[f]] == ell, ell] <- 1
@@ -89,3 +121,4 @@ compare_records_BK <- function(df_1, df_2, fields,
 #     }
 #   }) %>%
 #   do.call(data.frame, .)
+
