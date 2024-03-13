94f8005a699ab1561ef85de70b394480c75ba541
briankundinger <briankundinger@gmail.com>
2024-03-13 11:10:42 -0400

    updated code

--- R/estimate_links.R
+++ R/estimate_links.R
@@ -2,25 +2,7 @@
 #'
 estimate_links<- function(out, hash, l_FNM=1, l_FM1=1, l_FM2=2, l_R=Inf,
                           nonmatch_label = "zero", resolve = T){
-  #
-  # Adapted from BRL::linkrecords. See https://CRAN.R-project.org/package=BRL
-  #
-
   # "out" can be the output from either fabl or vabl.
-
-
-
-  # - positive losses
-  C0 <- (l_FNM > 0) & (l_FM1 > 0) & (l_FM2 > 0) & (l_R > 0)
-  # - conditions of Theorem 1 of Sadinle (2017)
-  C1 <- (l_R == Inf) & (l_FNM <= l_FM1) & (l_FNM + l_FM1 <= l_FM2)
-  # - conditions of Theorem 2 of Sadinle (2017)
-  # C2 <- ((l_FM2 >= l_FM1) & (l_FM1 >= 2*l_R)) | ((l_FM1 >= l_FNM) & (l_FM2 >= l_FM1 + l_FNM))
-  # - conditions of Theorem 3 of Sadinle (2017)
-  C3 <- (l_FM2 >= l_FM1) & (l_FM1 >= 2*l_R) & (l_FNM >= 2*l_R)
-  # check we can handle the specified losses
-  if(!C0) stop("Losses need to be positive")
-  if(!(C1 | C3)) stop("Losses must satisfy Theorem 1 or Theorem 3")
 
 
   if(names(out)[1] == "Z"){
@@ -61,8 +43,6 @@ estimate_links<- function(out, hash, l_FNM=1, l_FM1=1, l_FM2=2, l_R=Inf,
         unname()
 
       data.frame(record, prob)
-
-      #record
     })
 
     max_prob <- lapply(possible_records, function(x){
@@ -76,7 +56,7 @@ estimate_links<- function(out, hash, l_FNM=1, l_FM1=1, l_FM2=2, l_R=Inf,
     link_indicator <- best_match > 0
   }
 
-  if(C1){# if not using reject option and conditions of Theorem 1
+  if(l_R == Inf){# if not using reject option and conditions of Theorem 1
 
     if (nonmatch_label == "n_1 + j"){
       Z_hat <- (n1+1):(n1+n2)
@@ -92,7 +72,7 @@ estimate_links<- function(out, hash, l_FNM=1, l_FM1=1, l_FM2=2, l_R=Inf,
 
   }
 
-  if(C3){
+  if(l_R < Inf){
       Z_hat <- rep(-1, n2) # represents the reject option
       threshold <- 1 - l_R/l_FM1 +
         (l_FM2-l_FM1)*(1 - prob_no_link - prob_best_match) / l_FM1
@@ -135,123 +115,3 @@ estimate_links<- function(out, hash, l_FNM=1, l_FM1=1, l_FM2=2, l_R=Inf,
   return(list(Z_hat = Z_hat,
               prob = prob_best_match))
 }
-
-# estimate_links<- function(out, hash, l_FNM=1, l_FM1=1, l_FM2=2, l_R=Inf,
-#                           nonmatch_label = "zero", resolve = T){
-#
-#   n1 <- hash$n1
-#   n2 <- hash$n2
-#   if(names(out)[1] == "Z"){
-#     Z_samps <- out$Z
-#     Z_hat <- BRL::linkRecords(Z_samps, hash$n1, l_FNM, l_FM1, l_FM2, l_R)
-#         n2 <- nrow(Z_samps)
-#
-#         Z_samps[Z_samps > n1+1] <- n1+1
-#
-#         samps <- ncol(Z_samps)
-#         probs <- apply(Z_samps, 1, function(x){
-#           table(x)/samps
-#         })
-#         prob_no_link <- sapply(probs, function(x){
-#           1 - sum(x[names(x) != n1 + 1])
-#         })
-#         Z_hat <- rep(0, n2)
-#         best_match <- sapply(probs, function(x){
-#           names(which.max(x))
-#         }) %>%
-#           as.numeric()
-#         prob_best_match <- sapply(probs, function(x){
-#           max(x)
-#         })
-#         link_indicator <- best_match < n1 + 1
-#   }
-#
-#   if(names(out)[1] == "pattern_weights"){
-#
-#         n2 <- hash$n2
-#         pattern_probs <- lapply(1:n2, function(j){
-#           out$pattern_weights/out$C[j]
-#         })
-#
-#         possible_records <- lapply(1:n2, function(j){
-#           record <- c(hash$flags[[j]]$eligible_records, 0)
-#           prob <- c(pattern_probs[[j]][hash$flags[[j]]$eligible_patterns],
-#                     exp(digamma(out$b_pi)) / out$C[j])
-#
-#           data.frame(record, prob)
-#         })
-#
-#         max_prob <- lapply(possible_records, function(x){
-#           x[which.max(x$prob), ]
-#         }) %>%
-#           do.call(rbind, .)
-#
-#         best_match <- max_prob$record
-#         prob_best_match <- max_prob$prob
-#         prob_no_link <- out$b_pi/out$C
-#         link_indicator <- best_match > 0
-#   }
-#
-#     if(C1){# if not using reject option and conditions of Theorem 1
-#
-#       if (nonmatch_label == "n_1 + j"){
-#         Z_hat <- (n1+1):(n1+n2)
-#       }
-#       if (nonmatch_label == "zero"){
-#         Z_hat <- rep(0, n2)
-#       }
-#       threshold <- l_FM1/(l_FM1+l_FNM) +
-#         (l_FM2-l_FM1-l_FNM)*(1 - prob_no_link - prob_best_match)/(l_FM1+l_FNM)
-#
-#       Z_hat[link_indicator & (prob_best_match > threshold)] <-
-#         best_match[link_indicator & (prob_best_match > threshold)]
-#
-#     }
-#
-#     if(C3){
-#         Z_hat <- rep(-1, n2) # represents the reject option
-#         threshold <- 1 - l_R/l_FM1 +
-#           (l_FM2-l_FM1)*(1 - prob_no_link - prob_best_match) / l_FM1
-#
-#         Z_hat[link_indicator & (prob_best_match > threshold) ] <-
-#           best_match[link_indicator & (prob_best_match > threshold) ]
-#         nonlink_indicator <- prob_no_link > 1 - l_R / l_FNM
-#
-#         if (nonmatch_label == "n_1 + j"){
-#           Z_hat[nonlink_indicator] <- ((n1+1):(n1+n2))[nonlink_indicator]
-#         }
-#         if (nonmatch_label == "zero"){
-#           Z_hat[nonlink_indicator] <- 0
-#         }
-#     }
-#
-#       # TODO: Write code for Theorem 2
-#
-#
-#     # Enforce one-to-one matching
-#     if (resolve == T){
-#       double_matches <- Z_hat[duplicated(Z_hat) & Z_hat > 0]
-#       if (l_R == Inf){
-#         to_resolve <- unlist(lapply(double_matches, function(x){
-#           df1_options <- which(Z_hat == x)
-#           df1_probs <- prob_best_match[df1_options]
-#           non_matches <- df1_options[-which.max(df1_probs)]
-#           non_matches
-#         }))
-#         Z_hat[to_resolve] <- 0
-#       } else {
-#         to_resolve <- unlist(lapply(double_matches, function(x){
-#           df1_options <- which(Z_hat == x)
-#           df1_options
-#         }))
-#         Z_hat[to_resolve] <- -1
-#       }
-#     }
-#
-#   return(list(Z_hat = Z_hat,
-#        prop = prob_best_match))
-#
-#
-# }
-
-
--- README.md
+++ README.md
@@ -6,32 +6,67 @@
 <!-- badges: start -->
 <!-- badges: end -->
 
-This is the repo for the package. We can put a vignette here.
+This repo implements the record linkage techniques proposed in
+Variational Beta Linkage (citation TBD).
 
 ## Installation
 
 You can install the development version of vabl like so:
 
 ``` r
-# FILL THIS IN! HOW CAN PEOPLE INSTALL YOUR DEV PACKAGE?
+devtools::install_github("briankundinger/vabl")
+#> Downloading GitHub repo briankundinger/vabl@HEAD
+#> rlang        (1.1.2  -> 1.1.3   ) [CRAN]
+#> glue         (1.6.2  -> 1.7.0   ) [CRAN]
+#> MatrixModels (0.5-2  -> 0.5-3   ) [CRAN]
+#> withr        (2.5.2  -> 3.0.0   ) [CRAN]
+#> mcmc         (0.9-7  -> 0.9-8   ) [CRAN]
+#> coda         (0.19-4 -> 0.19-4.1) [CRAN]
+#> tidyselect   (1.2.0  -> 1.2.1   ) [CRAN]
+#> MCMCpack     (1.6-3  -> 1.7-0   ) [CRAN]
+#> Installing 8 packages: rlang, glue, MatrixModels, withr, mcmc, coda, tidyselect, MCMCpack
+#> Installing packages into 'C:/Users/brian/AppData/Local/Temp/RtmpktMGV7/temp_libpath3fcc36e33f4b'
+#> (as 'lib' is unspecified)
+#> package 'rlang' successfully unpacked and MD5 sums checked
+#> package 'glue' successfully unpacked and MD5 sums checked
+#> package 'MatrixModels' successfully unpacked and MD5 sums checked
+#> package 'withr' successfully unpacked and MD5 sums checked
+#> package 'mcmc' successfully unpacked and MD5 sums checked
+#> package 'coda' successfully unpacked and MD5 sums checked
+#> package 'tidyselect' successfully unpacked and MD5 sums checked
+#> package 'MCMCpack' successfully unpacked and MD5 sums checked
+#> 
+#> The downloaded binary packages are in
+#>  C:\Users\brian\AppData\Local\Temp\RtmpkbiDgk\downloaded_packages
+#> ── R CMD build ─────────────────────────────────────────────────────────────────
+#>          checking for file 'C:\Users\brian\AppData\Local\Temp\RtmpkbiDgk\remotes4a044ba2e1c\briankundinger-vabl-38da6e5/DESCRIPTION' ...  ✔  checking for file 'C:\Users\brian\AppData\Local\Temp\RtmpkbiDgk\remotes4a044ba2e1c\briankundinger-vabl-38da6e5/DESCRIPTION'
+#>       ─  preparing 'vabl':
+#>    checking DESCRIPTION meta-information ...     checking DESCRIPTION meta-information ...   ✔  checking DESCRIPTION meta-information
+#>       ─  checking for LF line-endings in source and make files and shell scripts
+#>   ─  checking for empty or unneeded directories
+#>       ─  building 'vabl_0.0.0.9000.tar.gz'
+#>      
+#> 
+#> Installing package into 'C:/Users/brian/AppData/Local/Temp/RtmpktMGV7/temp_libpath3fcc36e33f4b'
+#> (as 'lib' is unspecified)
 ```
 
 ## Example
 
-This is a basic example which shows you how to solve a common problem:
+Add vignette?
 
 ``` r
 library(vabl)
-## basic example code
+#> Loading required package: dplyr
+#> Warning: package 'dplyr' was built under R version 4.3.2
+#> 
+#> Attaching package: 'dplyr'
+#> The following objects are masked from 'package:stats':
+#> 
+#>     filter, lag
+#> The following objects are masked from 'package:base':
+#> 
+#>     intersect, setdiff, setequal, union
 ```
 
-What is special about using `README.Rmd` instead of just `README.md`?
-You can include R chunks like so:
-
-You’ll still need to render `README.Rmd` regularly, to keep `README.md`
-up-to-date. `devtools::build_readme()` is handy for this.
-
-You can also embed plots, for example:
-
-In that case, don’t forget to commit and push the resulting figure
-files, so they display on GitHub and CRAN.
+<!-- devtools::build_readme() -->
